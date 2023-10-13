2f6ed259a2fb5f4228f982298d94128761cc825d
Eman Medhat <147799869+Emo2003@users.noreply.github.com>
2023-10-13 10:28:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int x;
+
+	cin>>x;
+
+	if(x%2==0)
+
+        cout<<"This number is even "<<endl;
+    else
+
+        cout<<"This number is odd "<<endl;
+    return 0;
+}
